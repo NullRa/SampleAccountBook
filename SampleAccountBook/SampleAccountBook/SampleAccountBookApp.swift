43b84e4b68b7d2f46f95313c4acaed4aac666b37
jyang <jTang@walkgame.com>
2022-09-29 17:49:30 +0800

    init porject

---
+++ SampleAccountBook/SampleAccountBook/SampleAccountBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAccountBookApp.swift
+//  SampleAccountBook
+//
+//  Created by Apple on 2022/9/29.
+//
+
+import SwiftUI
+
+@main
+struct SampleAccountBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
